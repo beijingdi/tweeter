349e4b654a5f12807e144554184eafec6c4e1d0e
Di <dwu233@gmail.com>
2022-04-26 17:57:40 +0000

    the counter will turn red when number of char exceeds the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,9 @@ $(document).ready(()=> {
   $("textarea").on("input", ()=> {
     charCounter = $("textarea").val().length;
     $(".counter").text(140 - charCounter);
-    
+    if (charCounter > 140) {
+      $(".counter").css('color','red');
+    }
   });
   // --- our code goes here ---
 });
